a42a57732340124d0d5ebe7b1588f22b0a018070
abureyanahmed <abureyanahmed@email.arizona.edu>
2025-12-16 21:53:30 -0700

    Add files via upload

--- utils.py
+++ utils.py
@@ -59,3 +59,44 @@ def graph_weight(G):
     wght += G[u][v]['weight']
   return wght
 
+def ccw(ax, ay, bx, by, cx, cy):
+    return (cy - ay) * (bx - ax) > (by - ay) * (cx - ax)
+
+def get_crossings(G, pos):
+  # --- Build coordinate arrays ---
+  edges = np.array(list(G.edges()))
+  E = len(edges)
+  x1 = np.array([pos[u][0] for u, v in edges])
+  y1 = np.array([pos[u][1] for u, v in edges])
+  x2 = np.array([pos[v][0] for u, v in edges])
+  y2 = np.array([pos[v][1] for u, v in edges])
+
+  # --- Generate all unique edge pairs ---
+  i_idx, j_idx = np.triu_indices(E, k=1)
+
+  # Filter out pairs that share a vertex
+  shared_vertex = (
+      (edges[i_idx, 0] == edges[j_idx, 0]) |
+      (edges[i_idx, 0] == edges[j_idx, 1]) |
+      (edges[i_idx, 1] == edges[j_idx, 0]) |
+      (edges[i_idx, 1] == edges[j_idx, 1])
+  )
+  i_idx, j_idx = i_idx[~shared_vertex], j_idx[~shared_vertex]
+
+  # --- Get coordinates for all pairs ---
+  x1_i, y1_i, x2_i, y2_i = x1[i_idx], y1[i_idx], x2[i_idx], y2[i_idx]
+  x1_j, y1_j, x2_j, y2_j = x1[j_idx], y1[j_idx], x2[j_idx], y2[j_idx]
+
+  ccw1 = ccw(x1_i, y1_i, x1_j, y1_j, x2_j, y2_j)
+  ccw2 = ccw(x2_i, y2_i, x1_j, y1_j, x2_j, y2_j)
+  ccw3 = ccw(x1_i, y1_i, x2_i, y2_i, x1_j, y1_j)
+  ccw4 = ccw(x1_i, y1_i, x2_i, y2_i, x2_j, y2_j)
+
+  intersects = (ccw1 != ccw2) & (ccw3 != ccw4)
+
+  # --- Extract the crossing pairs ---
+  crossings = np.column_stack((i_idx[intersects], j_idx[intersects]))
+  cross_pairs = [(tuple(edges[i]), tuple(edges[j])) for i, j in crossings]
+
+  print(f"Found {len(cross_pairs)} crossings (vectorized CPU version)")
+  return crossings, cross_pairs
